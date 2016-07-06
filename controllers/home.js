834ee815c573b9e4d0b2613dfd1c33d873821e56
王庆 <gggyd@live.cn>
2016-07-06 20:58:05 +0800

    feat: home,image page

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,27 @@
 module.exports = {
   index: function(req, res) {
-    res.send('The Home: index controller');
+    var viewModel = {
+      images: [
+        {
+          uniqueId: 1,
+          title: 'Sample Image 1',
+          description: '',
+          filename: 'sample1.jpg',
+          views: 0,
+          likes: 0,
+          timestamp: Date.now
+        },
+        {
+          uniqueId: 2,
+          title: 'Sample Image 2',
+          description: '',
+          filename: 'sample2.jpg',
+          views: 0,
+          likes: 0,
+          timestamp: Date.now
+        }
+    ]};
+
+    res.render('home/index', viewModel);
   }
 };
--- server/configure.js
+++ server/configure.js
@@ -6,13 +6,16 @@ var path = require('path'),
     cookieParser = require('cookie-parser'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
-    errorHandler = require('errorhandler');
+    errorHandler = require('errorhandler'),
+    multer = require('multer'),
+    moment = require('moment');
 
 module.exports = function(app) {
   app.use(morgan('dev'));
-  // app.use(bodyParser({
-  //   uploadDir: path.join(__dirname, 'public/upload/temp')
-  // }));
+  app.use(multer({
+    dest: path.join(__dirname, 'public/upload/temp')
+  }));
+  app.use(multer({dest:'./uploads/'}));
   app.use(bodyParser.urlencoded({
     'extended': true
   }));
@@ -25,6 +28,18 @@ module.exports = function(app) {
   if ('development' === app.get('env')) {
     app.use(errorHandler);
   }
+
+  app.engine('handlebars', exphbs.create({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts',
+    partialsDir: [app.get('views') + '/partials'],
+    helpers: {
+      timeago: function(timestamp) {
+        return moment(timestamp).startOf('minute').fromNow();
+      }
+    }
+  }).engine);
+  app.set('view engine', 'handlebars');
 
   return app;
 };
